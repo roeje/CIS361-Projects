55386621f17fe329f800c1b2a6d14b976c556235
Jesse Roe <Jesse Roe>
2016-09-12 21:00:13 -0400

    finished lab with test input and output files

--- lab1/lab1a.c
+++ lab1/lab1a.c
@@ -1,29 +1,46 @@
 #include <stdio.h>
 #include <ctype.h>
 
+/*
+	CIS 361 - Lab 1
+	Jesse Roe
+*/
+
+/*Main Function: Outputs the symbol sequence and word sequence counts*/
 int main () {
+
+	/*Initilize variables to store State, counts, and input characters*/
 	char ch;
 	int wordCount = 0;
 	int symbolCount = 0;
 	int state = 0;
 
 	printf ("Enter text (Ctrl-D to quit).\n");
+
 	while ( ch = getchar(), ch != EOF ) {
+
+		/*Check if space and if so, reset state*/
 		if ( isspace(ch) ) {
 			state = 0;
 		}
+
+		/*Check for alphanumerical and underscore characters*/
 		if ( (isalnum(ch) || ch == '_') && state != 1 ) {
 			state = 1;
 			wordCount++;
 		}
+
+		/*Check for Punctuation characters*/
 		if ( ispunct(ch) && ch != '_' && state != 2 ) {
 			state = 2;
 			symbolCount++;
 		}
-		// putchar(ch);		
+
+		putchar(ch);		
 	}
-	printf ("Your Symbol Count Was: %d -  Your Word Count Was: %d", symbolCount, wordCount);
-	// printf ("Your Word Count Was: %d\n", wordCount);
+
+	/*Display the results*/
+	printf ("\n# of Symbol Sequences Was: %d  &  Word Count Was: %d", symbolCount, wordCount);
 	
 	return 0;
 }
