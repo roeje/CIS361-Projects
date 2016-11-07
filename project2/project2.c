b853c047f764d90b16639d923059dd627a5d5e21
Jesse Roe <jessedroe@gmail.com>
2016-11-06 21:41:53 -0500

    add baisc file io to project2.c. create input output files

--- project2/project2.c
+++ project2/project2.c
@@ -10,20 +10,33 @@
 #include <string.h>
 #include <math.h>
 
-#include "queue.h"
-#include "list.c"
+// #include "queue.h"
+#include "list.h"
 
 /*Main method*/
 int main(int argc, char *argv[]) {
 
 	/*Define variables*/
 
+   FILE *fin, *fout;
+
+
    // Check to see if correct number of commandline parameter were passed
-	if (argc != 5) {
-	  printf ("Usage: project2.c option key infile, outfile\n");
+	if (argc != 3) {
+	  printf ("Usage: project2.c infile outfile\n");
 	  exit(1);
 	}
 
+   printf("File In: %s\n",argv[1]);
+   printf("File Out: %s\n",argv[2]);
+
+
+   // Attempt to open files passed from commandline
+	fin = fopen(argv[1], "r");
+	fout = fopen(argv[2], "w");
+
+
+   char * line = readline(fin);
 
 	return 0;
 }
